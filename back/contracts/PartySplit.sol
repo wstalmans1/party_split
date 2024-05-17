866d61125833d19bad064b0372fec2153aef3918
Wim Stalmans <wim.stalmans@gmail.com>
2024-05-17 23:10:02 +0200

    smart contract

--- back/contracts/PartySplit.sol
+++ back/contracts/PartySplit.sol
@@ -2,5 +2,18 @@
 pragma solidity ^0.8.4;
 
 contract Party {
-	
+    uint256 public deposit = 0;
+    uint256 public contribution;
+    address [] public participants;
+
+    constructor (uint256 _contribution) {
+        contribution = _contribution;        
+    }
+
+    function rsvp() external payable {
+        require (msg.value == contribution, "Incorrect contribution amount");
+        deposit += msg.value;
+        participants.push(msg.sender);        
+    }    
+
 }
