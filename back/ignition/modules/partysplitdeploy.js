ce763f1d1134cbc2d971c1a915ff91b57ee8bfb5
Wim Stalmans <wim.stalmans@gmail.com>
2024-05-18 12:11:23 +0200

    deployment

--- back/ignition/modules/partysplitdeploy.js
+++ back/ignition/modules/partysplitdeploy.js
@@ -2,7 +2,7 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 const { ethers } = require("ethers");
 
 module.exports = buildModule("PartyModule", (m) => {
-  const cost = ethers.utils.parseUnits("10000000000000000", "wei");
-  const party = m.contract("Party", [cost]);
+  //const cost = ethers.utils.parseUnits("10000000000000000");
+  const party = m.contract("Party", ["10000000000000000"]);
   return { party };
 });
