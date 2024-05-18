978994c5348f751c8b706a068eae47fa87afda2e
Wim Stalmans <wim.stalmans@gmail.com>
2024-05-18 10:02:47 +0200

    adapted smart contract

--- back/contracts/PartySplit.sol
+++ back/contracts/PartySplit.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.4;
 
 contract Party {
     uint256 public cost;
-    uint256 public totalDeposit = 0;
     mapping (address => uint256) public payments;
 
     constructor (uint256 _cost) {
@@ -14,7 +13,10 @@ contract Party {
         require (msg.value == cost, "Incorrect contribution amount");
         require (payments[msg.sender] == 0, "payment already done");
         payments[msg.sender] = msg.value;        
-        totalDeposit += msg.value;
+    }
+
+    function totalDeposits() external view returns (uint256) {
+        return address(this).balance;
     }    
 
 }
